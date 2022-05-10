d0b3244fb01d12afa551aa6598a545ac813aadc6
Andy Chan <andy.chan9930@gmail.com>
2022-05-10 14:54:12 -0400

    commit tree 198d19b8ae2356cfefcc60d9a1bb91a332e9c765

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
-  
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
   def show
     author = Author.find(params[:id])
 
@@ -7,7 +7,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
+    author = Author.create!(author_params)
 
     render json: author, status: :created
   end
@@ -18,4 +18,9 @@ class AuthorsController < ApplicationController
     params.permit(:email, :name)
   end
   
+  private 
+
+  def render_unprocessable_entity (invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
   def show
     post = Post.find(params[:id])
     
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -20,4 +20,9 @@ class PostsController < ApplicationController
     params.permit(:category, :content, :title)
   end
 
+  private
+
+  def render_unprocessable_entity (invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
 end
